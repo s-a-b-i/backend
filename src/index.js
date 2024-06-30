2b24ac4946ba3f6dcd7d5247b840c4e1121b5ae8
Sohaib <21ntucs1360@gmail.com>
2024-06-30 22:21:26 +0500

    mongodb_conn

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv"; 
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+
+connectDB();
+
+
+/*
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+
+    app.on('error', (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    
+  } catch (error) {
+    console.log("Error", error); 
+    throw error;
+  }
+})();
+*/
