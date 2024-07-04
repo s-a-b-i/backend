c5f38332adfc02402b7a3c0c0811d6d41a67dc17
Sohaib <21ntucs1360@gmail.com>
2024-07-04 17:18:51 +0500

    Add new routes for user contellwes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,37 +1,48 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refereshAccessToken, registerUser } from "../controllers/user.conroller.js";
-import { upload } from "../middlewares/multer.middleware.js"
+import {
+  loginUser,
+  logoutUser,
+  refereshAccessToken,
+  registerUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserProfile,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.conroller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]) ,
-    registerUser
-)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-
-router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser);
 
 //secure routes
 
-router.route("/logout").post(verifyJWT , logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refereshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").post(verifyJWT, updateUserProfile);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").post(verifyJWT, getWatchHistory);
 
-
-
-
-
-
-export  { router }
+export { router };
